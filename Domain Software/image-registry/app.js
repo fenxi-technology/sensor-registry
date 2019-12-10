e56ab7e17e71f3c0cb497d403421680a7c18fce8
“csjxhyzq <616756618@qq.com>
2019-12-10 15:19:08 +0800

    change images name

--- Domain Software/image-registry/app.js
+++ Domain Software/image-registry/app.js
@@ -14,14 +14,15 @@ var serveIndex = require('serve-index');
 var app = express();
 app.use(fileUpload());
 
-app.get('/ping', function(req, res) {
+app.get('/ping', function (req, res) {
   res.send('pong');
 });
 
-app.post('/upload', function(req, res) {
+app.post('/upload', function (req, res) {
   let sampleFile;
   let uploadPath;
-
+  let sensorName;
+  console.log("req");
   if (!req.files || Object.keys(req.files).length === 0) {
     res.status(400).send('No files were uploaded.');
     return;
@@ -30,11 +31,17 @@ app.post('/upload', function(req, res) {
   console.log('req.files >>>', req.files); // eslint-disable-line
 
   sampleFile = req.files.sampleFile;
+  //sensorname
+  sensorName = req.body.sensorName;
+  //sensortype
+  var s_name = sampleFile.name;
+  var index = s_name.lastIndexOf(".");
+  var imagetype = s_name.substring(index + 1, s_name.length);
 
-  uploadPath = __dirname + '/uploads/img/' + sampleFile.name;
-  webPath = '/uploads/img/' + sampleFile.name;
+  uploadPath = __dirname + '/uploads/img/' + sensorName + `.` + imagetype;
+  webPath = '/uploads/img/' + sensorName + `.` + imagetype;
 
-  sampleFile.mv(uploadPath, function(err) {
+  sampleFile.mv(uploadPath, function (err) {
     if (err) {
       return res.status(500).send(err);
     }
@@ -47,10 +54,10 @@ app.post('/upload', function(req, res) {
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // view engine setup
@@ -64,7 +71,7 @@ app.use(logger('dev'));
 //app.use(cookieParser());
 
 // https://github.com/expressjs/serve-index
-app.use('/uploads', express.static('uploads'), serveIndex('uploads', {'icons': true}))
+app.use('/uploads', express.static('uploads'), serveIndex('uploads', { 'icons': true }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -72,12 +79,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
